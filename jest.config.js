fdbfa59e3bf14ebd7f985e39c3286e2ffc871ed1
Juan <juanorellanawork2019@gmail.com>
2025-11-11 22:13:34 -0500

    Add CI/CD pipeline - GitHub Actions workflow for automated testing

--- jest.config.js
+++ jest.config.js
@@ -2,14 +2,19 @@ module.exports = {
   testEnvironment: 'node',
   coverageDirectory: 'coverage',
   collectCoverageFrom: [
-    '**/*.js',
+    'server4_app.js',
+    'social_routes.js',
+    'api/**/*.js',
     '!**/node_modules/**',
     '!**/tests/**',
     '!**/coverage/**',
+    '!jest.config.js',
+    '!playwright.config.js',
   ],
   testMatch: [
     '**/tests/**/*.test.js'
   ],
   verbose: true,
   testTimeout: 10000,
+  coverageReporters: ['text', 'html', 'json-summary'],
 };
